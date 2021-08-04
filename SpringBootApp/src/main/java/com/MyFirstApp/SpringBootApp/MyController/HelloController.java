dbbfc10d624048cbb45d5c8030ab3529808b744f
Shashidhar <Shashidhar@10.92.200.42>
2021-08-04 11:47:43 +0530

    First Commit - Uploding Full Project

---
+++ SpringBootApp/src/main/java/com/MyFirstApp/SpringBootApp/MyController/HelloController.java
@@ -0,0 +1,15 @@
+package com.MyFirstApp.SpringBootApp.MyController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String HelloWorld()
+	{
+		return "Welcome to MyFirstSpring App";
+	}
+	
+}
